6037d46c1cb24093e2924efe63df267ce032eef6
robstallion <robertefrancis18@gmail.com>
2017-12-31 18:54:01 +0000

    replace comp_move and human_move with a more generic move function that can be used for both players #1

--- lib/TTT/game.ex
+++ lib/TTT/game.ex
@@ -1,39 +1,38 @@
 defmodule TTT.Game do
   alias TTT.Outcome
 
-  def make_move(pid, comp_move_pid, human_move_pid) do
+  def make_move(pid, move_pid) do
     receive do
       {:human, board, tile, team } ->
         updated_board = update_board(board, tile, team)
         case Outcome.check_outcome(updated_board) do
           :win ->
-            send(human_move_pid, :end)
+            send(move_pid, :end)
             send(pid, {:human_win, updated_board})
           :draw ->
             send(pid, {:draw, updated_board})
           :false ->
-            send(comp_move_pid, {self(), updated_board})
-            make_move(pid, comp_move_pid, human_move_pid)
+            send(move_pid, {self(), team, updated_board})
+            make_move(pid, move_pid)
         end
       {:comp, board, tile, team } ->
         updated_board = update_board(board, tile, team)
         case Outcome.check_outcome(updated_board) do
           :win ->
-            send(human_move_pid, :end)
+            send(move_pid, :end)
             send(pid, {:comp_win, updated_board})
           :draw ->
             send(pid, {:draw, updated_board})
           :false ->
-            send(human_move_pid, {self(), updated_board})
-            make_move(pid, comp_move_pid, human_move_pid)
+            send(move_pid, {self(), team, updated_board})
+            make_move(pid, move_pid)
         end
     end
   end
 
   def run(tile) do
-    comp_move_pid  = spawn(TTT.Game, :comp_move, [])
-    human_move_pid = spawn(TTT.Game, :human_move, [])
-    make_move_pid  = spawn(TTT.Game, :make_move, [self(), comp_move_pid, human_move_pid])
+    move_pid = spawn(TTT.Game, :move, [])
+    make_move_pid  = spawn(TTT.Game, :make_move, [self(), move_pid])
 
     send(make_move_pid, {:human, create_board(), tile, 1})
 
@@ -47,24 +46,19 @@ defmodule TTT.Game do
     end
   end
 
-  # figure out a good way to refator this and comp_move as basically the same
-  def human_move() do
+  def move() do
     receive do
-      {make_move_pid, board} ->
-        send(make_move_pid, { :human, board, get_player_move(board), 1 })
-        human_move()
-      :end ->
-        exit(:normal)
-    end
-  end
-
-  def comp_move() do
-    receive do
-      {make_move_pid, board} ->
-        send(make_move_pid, { :comp, board, get_comp_move(board), -1 })
-        comp_move()
-      :end ->
-        exit(:normal)
+      {make_move_pid, tile, board} ->
+        case tile do
+          -1 ->
+            send(make_move_pid, { :human, board, get_player_move(board), 1})
+            move()
+          1 ->
+            send(make_move_pid, { :comp, board, get_comp_move(board), -1 })
+            move()
+          :end ->
+            exit(:normal)
+        end
     end
   end
 
